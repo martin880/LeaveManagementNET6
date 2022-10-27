7a7e261d951aca2582aaa7f3642565f40f86f243
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-10-27 20:45:45 +0700

    Added Database Tables and Migrations and Implement Database Section

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+   
+    }
+}
