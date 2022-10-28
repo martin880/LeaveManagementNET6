c38e74c17d1a2f1d9381d97833993fd35628ed05
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-10-28 21:28:33 +0700

    Modified the crud screens and added datatables and sweetalert.

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Must not exceed 25 days")]
+        public int DefaultDays { get; set; }
+    }
+}
